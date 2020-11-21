68be4d1a9fc07c8268dff14839d3c283c6ea4cf3
numer <numer@DESKTOP-3S8M1EC>
2020-11-21 11:32:40 +0100

    Version 1 menu de opciones, que cada cual avise por el grupo que quiere hacer y en su opcion y ya le daremos un sentido, pero basicamente cosas sencillitas.

---
+++ src/ed_/Ed_.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed_;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author numer
+ */
+public class Ed_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner teclado=new Scanner(System.in);
+       int op;
+       do{
+           System.out.println("Introduce una opcion: ");
+           System.out.println("1: ");
+           System.out.println("2: ");
+           System.out.println("3: ");
+           op=teclado.nextInt();
+           switch(op){
+               case 1:
+               break;
+               case 2:
+               break;
+               case 3:
+               break;
+               default:
+               System.out.println("Se ha confundido de opcion: ");
+               break;
+               
+           }
+       }while(op!=3);
+    }
+    
+}
