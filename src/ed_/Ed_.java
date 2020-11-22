0b3e5c4de3c30f1c2e184145c07c90290a98e0a1
Lau23aldazabal <73839631+Lau23aldazabal@users.noreply.github.com>
2020-11-22 19:01:32 +0100

    Update Ed_.java

--- src/ed_/Ed_.java
+++ src/ed_/Ed_.java
@@ -21,14 +21,19 @@ public class Ed_ {
        int op;
        do{
            System.out.println("Introduce una opcion: ");
-           System.out.println("1: ");
+           System.out.println("1: La suma de dos numeros introducidor por teclado.");
            System.out.println("2: ");
            System.out.println("3: ");
            System.out.println("4: Salir");
            op=teclado.nextInt();
            switch(op){
                case 1 //Laura:
-                   
+                System.out.println("Introduce el primer numero: ");
+                int a=teclado.nextInt();
+                System.out.println("Introduce el segundo numero: ");
+                int b=teclado.nextInt();
+                int suma=a+b;
+                System.out.println("El resultado de: "+a+ " + "+b+" es: "+suma);
                break;
                case 2:  //Sara;
                break;
