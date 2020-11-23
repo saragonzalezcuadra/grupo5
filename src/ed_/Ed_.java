a5224c952fb84f9ec72a3faab39d22326026d45a
saragonzalezcuadra <73840067+saragonzalezcuadra@users.noreply.github.com>
2020-11-23 16:44:26 +0100

    Add files via upload

--- src/ed_/Ed_.java
+++ src/ed_/Ed_.java
@@ -17,42 +17,48 @@ public class Ed_ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner teclado=new Scanner(System.in);
-       int op;
-       do{
-           System.out.println("Introduce una opcion: ");
-           System.out.println("1: La suma de dos numeros introducidor por teclado.");
-           System.out.println("2: ");
-           System.out.println("3: Eleva él número a un número.");
-           System.out.println("4: Salir");
-           op=teclado.nextInt();
-           switch(op){
-               case 1 //Laura:
-                System.out.println("Introduce el primer numero: ");
-                int a=teclado.nextInt();
-                System.out.println("Introduce el segundo numero: ");
-                int b=teclado.nextInt();
-                int suma=a+b;
-                System.out.println("El resultado de: "+a+ " + "+b+" es: "+suma);
-               break;
-               case 2:  //Sara;
-               break;
-               case 3: //Carlos;
-                    Sytem.out.println("Escribe el numero ha elevar");
+        Scanner teclado = new Scanner(System.in);
+        int op;
+        do {
+            System.out.println("Menú de opciones: ");
+            System.out.println("\t1: Calcula la suma de dos números introducidor por teclado.");
+            System.out.println("\t2: Muestra la tabla de multiplicar de un número.");
+            System.out.println("\t3: Calcula la potencia de un número.");
+            System.out.println("\t4: Salir.");
+            System.out.print("Introduce la opción: ");
+            op = teclado.nextInt();
+            switch (op) {
+                case 1: //Laura:
+                    System.out.print("Introduce el primer numero: ");
+                    int a = teclado.nextInt();
+                    System.out.print("Introduce el segundo numero: ");
+                    int b = teclado.nextInt();
+                    int suma = a + b;
+                    System.out.println("El resultado de: " + a + " + " + b + " es: " + suma);
+                    break;
+                case 2:  //Sara González Cuadra
+                    System.out.print("Introduce el número: ");
+                    int n = teclado.nextInt();
+                    for (int i = 0; i <= 10; i++) {
+                        System.out.println(n + " * " + i + " = " + (n * i));
+                    }
+                    break;
+                case 3: //Carlos;
+                    System.out.println("Escribe el numero a elevar");
                     int num = teclado.nextInt();
-                    System.out.println("A que quieres elevar el " + num);
+                    System.out.println("A qué quieres elevar el " + num);
                     int aux = teclado.nextInt();
-                    System.out.println("El cuadrado de " + num + " es: " + Math.pow(num, aux));
-               break;
-               case 4:
-               System.out.println("Has salido con exito.");
-               break;
-               default:
-               System.out.println("Se ha confundido de opcion. ");
-               break;
-               
-           }
-       }while(op!=4);
+                    System.out.println(num + " elevado a " + aux + " es: " + Math.pow(num, aux));
+                    break;
+                case 4:
+                    System.out.println("Has salido con exito.");
+                    break;
+                default:
+                    System.out.println("Se ha confundido de opción. ");
+                    break;
+
+            }
+        } while (op != 4);
     }
-    
+
 }
